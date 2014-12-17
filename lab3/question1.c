6a0b439864f5a23e9b0f572a3f9118a990717677
Jinrui Wang <wangjinrui2013@gmail.com>
2014-12-17 16:27:28 +0800

    Finish lab3 question1

--- lab3/question1.c
+++ lab3/question1.c
@@ -8,6 +8,7 @@
 #include<stdio.h>
 
 #define TASK_COUNT 7
+#define MAX_DEADLINE 6
 
 typedef struct task
 {
@@ -24,7 +25,30 @@ task tasks[TASK_COUNT] = {{1, 4, 70}, {2, 2, 60}, \
                          };
 int A[TASK_COUNT] = {0};
 
+// Sort the A according to deadline
+void sortByDeadline(int count)
+{
+    int i = 0;
+    int j = 0;
+    int temp = 0;
 
+    for(i = 0; i < count-1; i++)
+    {
+        for(j = 0; j < count-1-i; j++)
+        {
+            if(tasks[A[j]].deadline > tasks[A[j+1]].deadline)
+            {
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp;
+            }
+        }
+    }
+
+    return;
+}
+
+// Sort the tasks according to weight
 void sortByWeight()
 {
     int i = 0;
@@ -47,65 +71,49 @@ void sortByWeight()
     return;
 }
 
-// This function will make array A be a right task scheduling 
-void greedy()
+// Get  independent subset A 
+int greedy()
 {
     int i = 0;
     int j = 0;
-    int count = 1;
-    int maxDeadline = 0;
-    int tempDeadline = 0;
-    int isExist = 0;
+    int maxDeadline = MAX_DEADLINE;
+    int recordCount[TASK_COUNT] = {0};
+    int count = 0;
+    int deadline = 0;
+    int flag = 0;
     int tail = TASK_COUNT-1;
 
-    for(i = 1; i < TASK_COUNT; i++)
+    sortByWeight();
+
+    for(i = 0; i < TASK_COUNT; i++)
     {
-        // Check if has same deadline task in A[]
-        isExist = 0;
-        for(j = 0; j < count; j++)
+        // Check if the task should be added into A[]
+        deadline = tasks[i].deadline;
+        flag = 0; 
+        for(j = deadline; j <= maxDeadline; j++)
         {
-            if(tasks[A[j]].deadline == tasks[i].deadline)
+            // Task can not added into A[]
+            if(recordCount[j]+1 > j)
             {
-                isExist = 1;
+                flag = 1;
+                // Record the task in the tail of A[]
                 A[tail--] = i;
                 break;
             }
         }
-        if(isExist)
-            continue;
-        
-        // If current task's deadline bigger than the max deadline in A[]
-        // than add crrent task to A[]
-        if(tasks[A[count-1]].deadline < tasks[i].deadline)
+
+        // Task can added into A[]
+        if(!flag)
         {
             A[count++] = i;
-            continue;
-        }
-       
-        // Cheak if current task can add into A[]
-        count++;
-        if(tasks[A[count-1]].deadline >= count)
-        {
-            // Insert task to A[], check from tail to top
-            for(j = count-1; j >= 0; j--)
+            for(j = deadline; j < maxDeadline+1; j++)
             {
-                if(tasks[A[j]].deadline > tasks[i].deadline )
-                {
-                    A[j+1] = A[j];
-                }
-                else
-                {
-                    A[j] = i;
-                    break;
-                }
+                recordCount[j]++;
             }
-        }
-        else
-        {
-            A[tail--] = i;
-            count--;
-        }
+        }   
     }
+
+    return count;
 }
 
 void print()
@@ -115,14 +123,14 @@ void print()
     printf("Task    : ");
     for(i = 0; i< TASK_COUNT; i++)
     {
-        printf("%d ", tasks[i].id);
+        printf("%d  ", tasks[i].id);
     }
     printf("\n");
 
     printf("Deadline: ");
     for(i = 0; i< TASK_COUNT; i++)
     {
-        printf("%d ", tasks[i].deadline);
+        printf("%d  ", tasks[i].deadline);
     }
     printf("\n");
 
@@ -136,7 +144,7 @@ void print()
     printf("Sequence: ");
     for(i = 0; i< TASK_COUNT; i++)
     {
-        printf("%d ", tasks[A[i]].id);
+        printf("%d  ", tasks[A[i]].id);
     }
     printf("\n");
 
@@ -145,10 +153,10 @@ void print()
 
 void main()
 {
-    sortByWeight();
-    
-    greedy();
-    
+    int count = 0;
+
+    count = greedy();
+    sortByDeadline(count);
     print();
 
     return;
